6a68c485c8abf7cc7776953d38334f0dbc559de7
Himanshu Gauba <hgauba4@gmail.com>
2024-09-17 02:43:06 +0530

    exploring get,post and delete

--- src/app.js
+++ src/app.js
@@ -2,10 +2,21 @@ const express = require("express");
 const app = express();
 
 
+app.get("/user",(req,res)=>{
+    res.send({"name":"Himanshu","age":21});
+});
+
+app.post("/user",(req,res)=>{
+    res.send("Saving User Data")
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("Deleting User Data")
+})
 app.use("/test",(req,res)=>res.send("Hello World"));
-// app.use("",(req,res)=>{
-//     res.send("Dev Tinder learning");
-// })
+app.use("",(req,res)=>{
+    res.send("Dev Tinder learning");
+})
 
 app.listen(4000,()=>{
     console.log("Listening on port 4000");
