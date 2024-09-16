85f3a27bf7f991a5b14cc233c6d6412623eddd0e
Himanshu Gauba <himanshu.gauba@decisionpoint.in>
2024-09-17 01:50:07 +0530

    created an express app server

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+
+app.use("/test",(req,res)=>res.send("Hello World"));
+app.use("",(req,res)=>{
+    res.send("Dev Tinder learning");
+})
+
+app.listen(4000,()=>{
+    console.log("Listening on port 4000");
+});
