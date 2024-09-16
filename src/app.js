d613c21afdda06d85613e077f0db9a8654b6e29e
Himanshu Gauba <hgauba4@gmail.com>
2024-09-17 02:29:58 +0530

    route handler

--- src/app.js
+++ src/app.js
@@ -3,9 +3,9 @@ const app = express();
 
 
 app.use("/test",(req,res)=>res.send("Hello World"));
-app.use("",(req,res)=>{
-    res.send("Dev Tinder learning");
-})
+// app.use("",(req,res)=>{
+//     res.send("Dev Tinder learning");
+// })
 
 app.listen(4000,()=>{
     console.log("Listening on port 4000");
